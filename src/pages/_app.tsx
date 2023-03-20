19efef8e43a9feb798b34e7846f20f65552fe1d2
Hildebrando Ferreira <hildebrandoweb@gmail.com>
2023-03-20 10:27:26 -0300

    Ajustes no repositorio

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import "../../styles/global.scss";
+
+import type { AppProps } from "next/app";
+import { Header } from "../components/Header";
+
+import styles from "../../styles/app.module.scss";
+import { Player } from "../components/Player";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  );
+}
